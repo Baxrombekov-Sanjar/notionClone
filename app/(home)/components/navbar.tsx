aa8ef1cec8e815e7519c3cc79450099156172cbc
Baxrombekov-Sanjar <sanjarbaxrombekov3@gmail.com>
2024-04-12 16:15:29 +0500

    home page

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,11 +1,24 @@
+"use client";
+
 import React from "react";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/shared/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { useScrolled } from "@/hooks/use-scrolled";
+import { cn } from "@/lib/utils";
 
 export const Navbar = () => {
+  const scrolled = useScrolled();
+
+  console.log(scrolled);
+
   return (
-    <div className="z-50 bg-background fixed top-0 flex justify-between items-center w-full p-6">
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex justify-between items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
       <Logo />
       <div className="flex items-center gap-x-2">
         <Button size={"sm"} variant={"ghost"}>
